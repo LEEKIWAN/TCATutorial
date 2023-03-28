e881c4e3b4021bf2fd9feef2aa27184a2ab7d61c
이기완 <kiwan.lee@waiker.ai>
2023-03-28 17:42:42 +0900

    Initial Commit

---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by 이기완 on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
